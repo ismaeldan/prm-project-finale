acd4614cbb6cd563d2b3af989927e5e4fe166135
Ismael Dantas <62301040+ismaeldan@users.noreply.github.com>
2025-08-02 19:50:19 -0300

    Update index.tsx

--- src/pages/Services/ServicesSublimacao/index.tsx
+++ src/pages/Services/ServicesSublimacao/index.tsx
@@ -13,7 +13,7 @@ export function Sublimacao() {
   return (
     <main className={styles.main}>
       <div className={`${styles.service__item} container`}>
-        <img src={ImgSublimacao} alt="Imagem Sublimacao" />
+        <img src={ImgSublimacao} alt="" />
 
         <div id="sublimacao" className={styles.service__item__content}>
           <div className={styles.line}></div>
